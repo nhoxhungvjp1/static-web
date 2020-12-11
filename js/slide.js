aeec6ecb3efe78d2c90e0e38825768b6db77a954
nhoxhungvjp1 <74084773+nhoxhungvjp1@users.noreply.github.com>
2020-12-11 21:15:45 +0700

    Update slide.js

--- js/slide.js
+++ js/slide.js
@@ -1,22 +1,23 @@
-
+//lấy kích thước ảnh
 var KichThuoc = document.getElementsByClassName("slide")[0].clientWidth;
 var ChuyenSlide = document.getElementsByClassName("chuyen-slide")[0];
 var Img = ChuyenSlide.getElementsByTagName("img");
 var Max = KichThuoc * Img.length;
 Max -= KichThuoc;
 var Chuyen = 0;
+//tiến hành chuyển slide lên ảnh phía trước khi click vào mũi tên chuyển trái.
 function Next(){
 	if(Chuyen < Max) Chuyen += KichThuoc;
 	else Chuyen = 0;
 	ChuyenSlide.style.marginLeft = '-' + Chuyen + 'px';
 }
-
+//tiến hành chuyển slide về ảnh phía sau khi click vào mũi tên chuyển phải.
 function Back(){
 	if(Chuyen == 0) Chuyen = Max;
 	else Chuyen -= KichThuoc;
 	ChuyenSlide.style.marginLeft = '-' + Chuyen + 'px';
 }
-
+//tự động chuyển ảnh 
 setInterval(function(){
 	Next();
 }, 3000);
